cd592ced16f259515fe887c4300fd180c4236eb9
Marcin <mchyla@ur.edu.pl>
2018-04-11 16:33:18 +0200

    init

---
+++ kolo1/src/main/java/pl/edu/ur/kolo1/Main.java
@@ -0,0 +1,16 @@
+package pl.edu.ur.kolo1;
+
+/**
+ *
+ * @author mchyla
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO Uruchomienie zadań/stworzenie i działanie na obiektach
+    }
+    
+}
